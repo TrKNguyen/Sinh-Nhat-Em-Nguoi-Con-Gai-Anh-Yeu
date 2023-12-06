54b5ce214c37e6b1e63c752735aebc7ad8809954
TrKNguyen <nguyenusaco@gmail.com>
2023-12-06 23:43:06 +0700

    add

---
+++ config.js
@@ -0,0 +1,52 @@
+var config = {
+    texts: [
+        "Gửi Phùng Khánh Linh",
+        "Người con gái chăm chỉ, giỏi giang, xinh đẹp, ấm áp nhất vũ trụ của Trần Khôi Nguyên",
+        "Hôm nay là ngày sinh nhật em ấyyy",
+        "Là lần đầu tiên chúng ta đón sinh nhật cùng nhau",
+        "Xa quá",
+        "Khó khăn quá",
+        "Nhưng mà em và anh vẫn ở đây",
+        "Vậy là đủ rồi",
+        "Anh chỉ mong như vậy",
+        "Mùa đông này thật ấm",
+        "Khi có em ở bên cạnh",
+        "Chúc em",
+        "Sinh nhật",
+        "Thật hạnh phúc",
+        "Dù cho mai sau có như thế nào đi nữa",
+        "Em hay luôn là em và mãi tỏa sáng như bây giờ",
+    ],
+    /**
+     phải viết khớp dòng chữ với ảnh 
+     */
+    imgs: {
+        "Gửi Phùng Khánh Linh": "./imgs/anhbantim.png",
+        "Người con gái chăm chỉ, giỏi giang, xinh đẹp, ấm áp nhất vũ trụ của Trần Khôi Nguyên": "./imgs/emxinhqua_2.png",
+        "Hôm nay là ngày sinh nhật em ấyyy": "./imgs/emvoianh.png",
+        "Là lần đầu tiên chúng ta đón sinh nhật cùng nhau": "./imgs/emvoianh_2.jpg",
+        "Xa quá": "./imgs/emvoianh_hotay_gif.gif",
+        "Khó khăn quá": "./imgs/emvoianh_khokhan_thuc_su.png",
+        "Nhưng mà em và anh vẫn ở đây": "./imgs/emvoianh_dapvit.gif",
+        "Vậy là đủ rồi": "./imgs/emvoianh_chupanh_8_3.png",
+        "Anh chỉ mong như vậy": "./imgs/emvoianh_chup4anh_8_3.gif",
+        "Mùa đông này thật ấm": "./imgs/emvoianh_antaco.png",
+        "Khi có em ở bên cạnh": "./imgs/emvoianh_anbimbim.gif",
+        "Chúc em": "./imgs/emvoianh_stickercute.gif",
+        "Sinh nhật": "./imgs/emvoianh_doiminhladepnhat.png",
+        "Thật hạnh phúc": "./imgs/emquaxinhdep_voihoaanhtang_final.png",
+        "Dù cho mai sau có như thế nào đi nữa": "./imgs/emvoianh_minhquadep.png",
+        "Em hay luôn là em và mãi tỏa sáng như bây giờ": "./imgs/emquaxinhdep_gif_8s.gif"
+    },
+    desc: {
+        turn_on: "Start !",
+        play: "Lên nhạc",
+        bannar_coming: "Let's go",
+        balloons_flying: "Hình như thiêú cái gì đó",
+        cake_fadein: "Bánh ?",
+        light_candle: "Nến ?",
+        wish_message: "Chúc mừng sinh nhật em～",
+        story: "A MESSAGE FOR YOU"
+    },
+    loveText: 'Yêu em 🧡, người con gái tuyệt vời nhất của anh ~'
+};
